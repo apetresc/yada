88aedb23546532724a68292025d3ff54e0548df7
Adrian Petrescu <apetresc@gmail.com>
2019-06-27 22:34:21 -0400

    Import pathlib.Path directly
    Just to make lines shorter and prettier

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -1,8 +1,8 @@
 import sys
 if sys.version_info >= (3, 6):
-    import pathlib  # pylint: disable=import-error
+    from pathlib import Path  # pylint: disable=import-error
 else:
-    import pathlib2 as pathlib  # pylint: disable=import-error
+    from pathlib2 import Path  # pylint: disable=import-error
 
 import yada.config
 import yada.repo
@@ -14,14 +14,14 @@ home = yada.config.get_home()
 def test_repo_relative_to__default():
     repo = yada.repo.get_default_repo()
     
-    assert repo.path_relative_to(home / "vim") == pathlib.Path("../.local/share/yada/dot")
-    assert repo.path_relative_to(home / "vim/x") == pathlib.Path("../../.local/share/yada/dot")
-    assert repo.path_relative_to(home / "vim/") == pathlib.Path("../.local/share/yada/dot")
-    assert repo.path_relative_to(home / "a/b/c/d") == pathlib.Path("../../../../.local/share/yada/dot")
-    assert repo.path_relative_to(home / "") == pathlib.Path(".local/share/yada/dot")
-    assert repo.path_relative_to(home) == pathlib.Path(".local/share/yada/dot")
+    assert repo.path_relative_to(home / "vim") == Path("../.local/share/yada/dot")
+    assert repo.path_relative_to(home / "vim/x") == Path("../../.local/share/yada/dot")
+    assert repo.path_relative_to(home / "vim/") == Path("../.local/share/yada/dot")
+    assert repo.path_relative_to(home / "a/b/c/d") == Path("../../../../.local/share/yada/dot")
+    assert repo.path_relative_to(home / "") == Path(".local/share/yada/dot")
+    assert repo.path_relative_to(home) == Path(".local/share/yada/dot")
 
 def test_repo_relative_to__root():
     repo = yada.repo.get_default_repo(yada_home="/etc/yada")
 
-    assert repo.path_relative_to(home / "vim") == pathlib.Path("../../../etc/yada/dot")
+    assert repo.path_relative_to(home / "vim") == Path("../../../etc/yada/dot")
