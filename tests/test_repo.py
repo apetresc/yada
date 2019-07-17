01cf11d640e1c94e50a99f0ae57dfaeda7a4c22b
Adrian Petrescu <apetresc@gmail.com>
2019-07-17 02:03:14 -0400

    Accidentally hardcoded my username in unit tests

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -8,23 +8,24 @@ import yada.config
 import yada.repo
 
 
+user = yada.config.get_default_user_name()
 home = yada.config.get_home()
 
 
 def test_repo_relative_to__default():
     repo = yada.repo.get_default_repo()
     
-    assert repo.path_relative_to(home / "vim") == Path("../.local/share/yada/apetresc/dot")
-    assert repo.path_relative_to(home / "vim/x") == Path("../../.local/share/yada/apetresc/dot")
-    assert repo.path_relative_to(home / "vim/") == Path("../.local/share/yada/apetresc/dot")
-    assert repo.path_relative_to(home / "a/b/c/d") == Path("../../../../.local/share/yada/apetresc/dot")
-    assert repo.path_relative_to(home / "") == Path(".local/share/yada/apetresc/dot")
-    assert repo.path_relative_to(home) == Path(".local/share/yada/apetresc/dot")
+    assert repo.path_relative_to(home / "vim") == Path("../.local/share/yada/{user}/dot".format(user=user))
+    assert repo.path_relative_to(home / "vim/x") == Path("../../.local/share/yada/{user}/dot".format(user=user))
+    assert repo.path_relative_to(home / "vim/") == Path("../.local/share/yada/{user}/dot".format(user=user))
+    assert repo.path_relative_to(home / "a/b/c/d") == Path("../../../../.local/share/yada/{user}/dot".format(user=user))
+    assert repo.path_relative_to(home / "") == Path(".local/share/yada/{user}/dot".format(user=user))
+    assert repo.path_relative_to(home) == Path(".local/share/yada/{user}/dot".format(user=user))
 
 def test_repo_relative_to__root():
     repo = yada.repo.get_default_repo(yada_home="/etc/yada")
 
-    assert repo.path_relative_to(home / "vim") == Path("../../../etc/yada/apetresc/dot")
+    assert repo.path_relative_to(home / "vim") == Path("../../../etc/yada/{user}/dot".format(user=user))
 
 
 def test_module_relative_to():
